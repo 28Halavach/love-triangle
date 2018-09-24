100cf4ee83ed1b1fb45270e8c88fe34673cde13f
28Halavach <sasha2015to@gmail.com>
2018-09-24 18:57:35 +0300

    28Halavach do love-triangles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+// функция принимает в качестве аргумента массив занчений
+module.exports = function getLoveTrianglesCount(array =[]) {
+	
+	// где len - длина массива
+	// k - переменная счётчик определяющая количество вхождений
+	let k = 0, len = array.length, a, b, c;
+
+	for ( let i = 0; i < len; i++) {
+		
+		a = array[i];
+		b = array[a - 1];
+		c = array[b - 1];
+		
+		if( c === i + 1 && b > c && b > a){
+			k++;	
+		};
+		
+	};
+	//ответ количество наших любовных треугольников
+	return k;	
 };
